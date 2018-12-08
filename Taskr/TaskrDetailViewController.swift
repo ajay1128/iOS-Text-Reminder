df5c864465993ba4b0ec44ebca15b4db034ef6b1
ajay1128 <32371218+ajay1128@users.noreply.github.com>
2018-12-07 21:59:36 -0600

    added ability to access contacts

--- Taskr/TaskrDetailViewController.swift
+++ Taskr/TaskrDetailViewController.swift
@@ -1,17 +1,16 @@
 import UIKit
 import Alamofire
+import ContactsUI
+import Contacts
 
-class TaskrDetailViewController: UIViewController {
+class TaskrDetailViewController: UIViewController,CNContactPickerDelegate {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var deadlineDatePicker: UIDatePicker!
-    
     @IBOutlet weak var hostName: UITextField?
-    
-    
     @IBOutlet weak var phoneNumber: UITextField?
-    
     @IBOutlet weak var assignedTo: UITextField?
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,12 +21,14 @@ class TaskrDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //saves tasks
     @IBAction func save(_ sender: AnyObject) {
         let task = Task(ID: UUID().uuidString, title: titleTextField.text!,deadline: deadlineDatePicker.date, hostname: hostName?.text ?? " ", phone: phoneNumber?.text ?? " ", assignee: assignedTo?.text ?? " ")
         TaskList.sharedInstance.addTask(task)
         self.navigationController?.popToRootViewController(animated: true)
     }
     
+    //sendData - sends text messages to assignee
     @IBAction func sendData(_ sender: AnyObject) {
         let dateFormatter = DateFormatter()
         
@@ -50,7 +51,55 @@ class TaskrDetailViewController: UIViewController {
         }
     }
     
+    //accesses contacts app
+    @IBAction func AccessContacts(_ sender: Any) {
+        let entityType = CNEntityType.contacts
+        let authStat = CNContactStore.authorizationStatus(for: entityType)
+        if authStat == CNAuthorizationStatus.notDetermined{
+            let store = CNContactStore.init()
+            store.requestAccess(for: entityType, completionHandler: { (success,nil) in
+                
+                if success {
+                    self.openContacts()
+                }
+                else{
+                    print("access denied")
+                }
+            })
+            
+        }
+        else if authStat == CNAuthorizationStatus.authorized {
+            self.openContacts()
+        }
+    }
     
+    //opens contacts
+    func openContacts() {
+        let contactPicker = CNContactPickerViewController.init()
+        contactPicker.delegate = self
+        self.present(contactPicker, animated: true, completion: nil)
+    }
+    
+    //did not pick contact
+    func contactPickerDidCancel(_ picker: CNContactPickerViewController){
+        picker.dismiss(animated: true){
+        }
+    }
+    
+    //picks a contact
+    //assigns contact name to assignee field
+    //assigns phone number to phone number field
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact){
+        let fullName = "\(contact.givenName)\(contact.familyName)"
+        var phoneNo = "not available"
+        let phoneString = ((((contact.phoneNumbers[0] as AnyObject).value(forKey: "labelValuePair") as AnyObject).value(forKey: "value") as AnyObject).value(forKey: "stringValue"))
+        phoneNo = phoneString! as! String
+        self.assignedTo?.text = "\(fullName)"
+        self.phoneNumber?.text = "\(phoneNo)"
+    }
 }
+    
+    
+
 
 
